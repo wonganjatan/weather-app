fb7ec71538e28c9ffbaf3892bd4e89cf2b476657
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-11-13 21:15:56 +1100

    add simple debugger

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 const form = document.getElementById("weather-form")
 const resultDiv = document.getElementById("weather-result")
+
+form.addEventListener("submit", () => {
+    const city = document.getElementById("location").value
+
+    console.log(city)
+})
